990d1c5d5e4b8704629409885b91e4eec96a43eb
OriginCode <origincoder@yahoo.com>
2019-08-08 19:03:49 +0800

    add deghandler

--- main.py
+++ main.py
@@ -16,10 +16,36 @@ logger = logging.getLogger(__name__)
 config = configparser.ConfigParser()
 config.read('./config.ini')
 
-owm = pyowm.OWM(API_key=config['BOT']['APPID'])
+owm = pyowm.OWM(API_key=config['OWM']['APPID'])
 
 
-def weather(update, context):
+def __deghandler__(deg):
+    if 330 <= deg < 360 or 0 <= deg < 30:
+        return "N"
+
+    elif 30 <= deg < 60:
+        return "NE"
+    
+    elif 60 <= deg < 120:
+        return "E"
+    
+    elif 120 <= deg < 150:
+        return "SE"
+    
+    elif 150 <= deg < 210:
+        return "S"
+    
+    elif 210 <= deg < 240:
+        return "SW"
+    
+    elif 240 <= deg < 300:
+        return "W"
+    
+    elif 300 <= deg < 330:
+        return "NW"
+
+
+def owmhandler(update, context):
     city = ""
     for i in context.args:
         if not i.isdigit():
@@ -33,10 +59,14 @@ def weather(update, context):
     else:
         obs = owm.weather_at_places(city, searchtype='accurate', limit=3)
 
+    if len(obs) == 0:
+        update.message.reply_text(
+            '*Invalid City Name!*', parse_mode=ParseMode.MARKDOWN)
+
     for i in range(len(obs)):
         w = obs[i].get_weather()
         l = obs[i].get_location()
-        
+
         country = l.get_country()
         city = l.get_name()
         lon = l.get_lon()
@@ -54,10 +84,8 @@ def weather(update, context):
             '*Humidity* %d%%\n'
             '*Wind Speed* %d m/s\n'
             '*Wind Degree* %d°'
-            % (country, city, lon, lat, weather, temp, humidity, wind_speed, wind_deg),
+            % (country, city, lon, lat, weather, temp, humidity, wind_speed, __deghandler__(wind_deg)),
             parse_mode=ParseMode.MARKDOWN)
-
-    sleep(1)
 
 
 def error(update, context):
@@ -66,9 +94,11 @@ def error(update, context):
 
 def main():
     updater = Updater(config['BOT']['TOKEN'], use_context=True)
+
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler("weather", weather, pass_args=True))
+    dp.add_handler(CommandHandler("weather", owmhandler, pass_args=True))
     dp.add_error_handler(error)
+
     updater.start_polling()
     updater.idle()
 
