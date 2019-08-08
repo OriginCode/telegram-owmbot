b51b6c0c1d20ef229570e2919eb926ddd9d88416
OriginCode <origincoder@yahoo.com>
2019-08-08 19:13:21 +0800

    btr format output

--- main.py
+++ main.py
@@ -83,8 +83,8 @@ def owmhandler(update, context):
             '*Current Temperature* %d °C\n'
             '*Humidity* %d%%\n'
             '*Wind Speed* %d m/s\n'
-            '*Wind Degree* %s°'
-            % (country, city, lon, lat, weather, temp, humidity, wind_speed, __deghandler__(wind_deg)),
+            '*Wind Degree* %s %d°'
+            % (country, city, lon, lat, weather, temp, humidity, wind_speed, __deghandler__(wind_deg), wind_deg),
             parse_mode=ParseMode.MARKDOWN)
 
 
