8c5efbc11fe23721f9b0c194c9015a0d89f70f25
OriginCode <origincoder@yahoo.com>
2019-08-15 00:56:59 +0800

    add some info commands

--- main.py
+++ main.py
@@ -16,8 +16,6 @@ logger = logging.getLogger(__name__)
 config = configparser.ConfigParser()
 config.read('./config.ini')
 
-owm = pyowm.OWM(API_key=config['OWM']['APPID'])
-
 
 def __deghandler__(deg):
     if 330 <= deg <= 360 or 0 <= deg < 30:
@@ -25,67 +23,82 @@ def __deghandler__(deg):
 
     elif 30 <= deg < 60:
         return "NE"
-    
+
     elif 60 <= deg < 120:
         return "E"
-    
+
     elif 120 <= deg < 150:
         return "SE"
-    
+
     elif 150 <= deg < 210:
         return "S"
-    
+
     elif 210 <= deg < 240:
         return "SW"
-    
+
     elif 240 <= deg < 300:
         return "W"
-    
+
     elif 300 <= deg < 330:
         return "NW"
 
 
-def owmhandler(update, context):
-    city = ""
-    for i in context.args:
-        if not i.isdigit():
-            city += i
-            city += " "
-    city = city[:len(city) - 1]
+class cmd():
+    def __init__(self, owmcfg):
+        self.owmcfg = owmcfg
 
-    if context.args[len(context.args) - 1].isdigit():
-        lim = int(context.args[len(context.args) - 1])
-        obs = owm.weather_at_places(city, searchtype='accurate', limit=lim)
-    else:
-        obs = owm.weather_at_places(city, searchtype='accurate', limit=3)
+    def owmhandler(self, update, context):
+        owm = pyowm.OWM(API_key=self.owmcfg['APPID'])
+        city = ""
+        for i in context.args:
+            if not i.isdigit():
+                city += i
+                city += " "
+        city = city[:len(city) - 1]
 
-    if len(obs) == 0:
-        update.message.reply_text(
-            '*Invalid City Name!*', parse_mode=ParseMode.MARKDOWN)
+        if context.args[len(context.args) - 1].isdigit():
+            lim = int(context.args[len(context.args) - 1])
+            obs = owm.weather_at_places(city, searchtype='accurate', limit=lim)
+        else:
+            obs = owm.weather_at_places(city, searchtype='accurate', limit=3)
 
-    for i in range(len(obs)):
-        w = obs[i].get_weather()
-        l = obs[i].get_location()
+        if len(obs) == 0 or city == "":
+            update.message.reply_text(
+                '*Invalid City Name!*', parse_mode=ParseMode.MARKDOWN)
 
-        country = l.get_country()
-        city = l.get_name()
-        lon = l.get_lon()
-        lat = l.get_lat()
-        weather = w.get_status()
-        temp = w.get_temperature(unit='celsius')['temp']
-        humidity = w.get_humidity()
-        wind_speed = w.get_wind()['speed']
-        wind_deg = w.get_wind()['deg']
+        for i in range(len(obs)):
+            w = obs[i].get_weather()
+            l = obs[i].get_location()
 
-        update.message.reply_text(
-            '\[ %s - _%s_ (lon:%.3f lat:%.3f) ]\n'
-            '*Weather* %s\n'
-            '*Current Temperature* %d °C\n'
-            '*Humidity* %d%%\n'
-            '*Wind Speed* %d m/s\n'
-            '*Wind Degree* %s %d°'
-            % (country, city, lon, lat, weather, temp, humidity, wind_speed, __deghandler__(wind_deg), wind_deg),
-            parse_mode=ParseMode.MARKDOWN)
+            country = l.get_country()
+            city = l.get_name()
+            lon = l.get_lon()
+            lat = l.get_lat()
+            weather = w.get_status()
+            temp = w.get_temperature(unit='celsius')['temp']
+            humidity = w.get_humidity()
+            wind_speed = w.get_wind()['speed']
+            wind_deg = w.get_wind()['deg']
+
+            update.message.reply_text(
+                '\[ %s - _%s_ (lon:%.3f lat:%.3f) ]\n'
+                '*Weather* %s\n'
+                '*Current Temperature* %d °C\n'
+                '*Humidity* %d%%\n'
+                '*Wind Speed* %d m/s\n'
+                '*Wind Degree* %s %d°'
+                % (country, city, lon, lat, weather, temp, humidity, wind_speed, __deghandler__(wind_deg), wind_deg),
+                parse_mode=ParseMode.MARKDOWN)
+
+
+def version(update, context):
+    update.message.reply_text(
+        '`LIBOC v18, PROJEKT WEATHERBOT, PRE_RELEASE V2 BETA 1`', parse_mode=ParseMode.MARKDOWN)
+
+
+def about(update, context):
+    update.message.reply_text(
+        'GITHUB: https://github.com/OriginCode/telegram-owmbot')
 
 
 def error(update, context):
@@ -96,7 +109,10 @@ def main():
     updater = Updater(config['BOT']['TOKEN'], use_context=True)
 
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler("weather", owmhandler, pass_args=True))
+    dp.add_handler(CommandHandler('weather', cmd(config['OWM']).owmhandler,
+                                  pass_args=True))
+    dp.add_handler(CommandHandler('version', version, pass_args=False))
+    dp.add_handler(CommandHandler('about', about, pass_args=False))
     dp.add_error_handler(error)
 
     updater.start_polling()
